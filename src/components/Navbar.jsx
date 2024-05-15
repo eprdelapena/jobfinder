a67304ac514f2f541f232c66c0267c651220e3d8
Edrian Paulo <eprdelapena@gmail.com>
2024-05-15 12:10:49 -0700

    Navbar stage

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from '../assets/images/logo.png';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="bg-indigo-700 border-b border-indigo-500">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div className="flex flex-1 items-center md:justify-between justify-center">
+
+              <a className="flex flex-shrink-0 items-center mr-4" href="">  
+                <img className="h-10 w-auto" src={logo} alt="React Jobs" />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2"> React Jobs </span>
+              </a>
+              
+              <div className="md:ml-auto">
+                <a className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" href=""> Home </a>
+                <a className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" href=""> Jobs </a>
+                <a className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" href=""> Add job</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
