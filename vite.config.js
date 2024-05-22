7a20cf98a6e15ee76c0c67cc9b4e37347e7ad0a0
Edrian Paulo <eprdelapena@gmail.com>
2024-05-22 08:56:32 -0700

    first main

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], 
-  base: "/jobfinder/",
+  plugins: [react()],
   server: {
     port: 3000, 
     proxy: { // type this
