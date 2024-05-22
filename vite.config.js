af3c2af15e6bda06083fecf75a88f2680eef773c
Edrian Paulo <eprdelapena@gmail.com>
2024-05-22 07:40:00 -0700

    Publish

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
+  base: "/vite-deploy/",
   server: {
     port: 3000, 
     proxy: { // type this
