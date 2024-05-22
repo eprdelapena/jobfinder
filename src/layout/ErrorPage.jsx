42f3983f74388ef578b6440de68c76619262dd5f
Edrian Paulo <eprdelapena@gmail.com>
2024-05-22 09:15:45 -0700

    Finals

--- src/layout/ErrorPage.jsx
+++ src/layout/ErrorPage.jsx
@@ -1,11 +1,20 @@
 import Errors from "../components/Errors"
 
 import React from 'react'
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import HomeCards from "../components/HomeCards"
+import JobListing from "../components/JobListing"
+import BrowseJobs from "../components/BrowseJobs"
 
 const ErrorPage = () => {
   return (
     <>
-        <Errors />
+        <Navbar />
+        <Hero />
+        <HomeCards />
+        <JobListing />
+        <BrowseJobs />
     </>
   )
 }
