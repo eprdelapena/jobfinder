87bfdcbcd99c8edc2c29bd6d4fe0dd3ac8359299
Edrian Paulo <eprdelapena@gmail.com>
2024-05-22 01:20:48 -0700

    Final

--- src/layout/SingleJobPage.jsx
+++ src/layout/SingleJobPage.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
 import { deleteData } from '../function/deletedata';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SingleJobPage = () => {
   let params = useParams().profileID;
@@ -97,10 +98,7 @@ const SingleJobPage = () => {
 
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-              <a
-                href="/add-job.html"
-                className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</a>
+              <Link to={`/editjob/${params}`} className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"> Edit Job </Link>
               <button
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 onClick={deletes}
